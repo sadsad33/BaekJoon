6fdfeda8c0bd148d864fffaf954d60e71a6fa9d2
sadsad33 <bonhyuk33@naver.com>
2024-01-10 21:53:17 +0900

    12869

---
+++ 12869/main.cpp
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+int n, min;
+int arr[3], original[3], damage[3] = { 9, 3, 1 };
+void DFS(int index, int* attackCount) {
+	int succeed = 0, fail = 0;
+
+	for (int i = index; i < index + n; i++) {
+		arr[i % n] -= damage[i - index];
+		if (arr[i % n] < 1) succeed = 1;
+		if (arr[i % n] >= 1) fail = 1;
+	}
+	(*attackCount)++;
+
+	if (min != 1 << 30 && *attackCount >= min) return;
+
+	if (succeed && !fail) {
+		min = min > *attackCount ? *attackCount : min;
+		return;
+	}
+
+	for (int i = index; i < index + n; i++) {
+		DFS(i % n, attackCount);
+		(*attackCount)--;
+		for (int j = index; j < index + n; j++) {
+			arr[j % n] += damage[j - index];
+		}
+	}
+}
+
+int main(void) {
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &original[i]);
+		arr[i] = original[i];
+	}
+	min = 1 << 30;
+	for (int i = 0; i < n; i++) {
+		int attackCount = 0;
+		DFS(i, &attackCount);
+	}
+	printf("%d", min);
+	return 0;
+}
--- 12869_TLE/main.cpp
+++ 12869_TLE/main.cpp
@@ -1,44 +1,40 @@
 // 12869 시간초과
 #define _CRT_SECURE_NO_WARNINGS
 #include<iostream>
-int n, min;
-int arr[3], damage[3] = { 9, 3, 1 };
-void DFS(int index, int* attackCount) {
-	int succeed = 0, fail = 0;
-
-	for (int i = index; i < index + n; i++) {
-		arr[i % n] -= damage[i - index];
-		if (arr[i % n] < 1) succeed = 1;
-		if (arr[i % n] >= 1) fail = 1;
-	}
+int n, minCnt;
+int arr[3], visit[60][3], damage1[6] = { 9,9,3,3,1,1 }, damage2[6] = { 3,1,1,9,3,9 }, damage3[6] = { 1,3,9,1,9,3 };
+void DFS(int damageIndex, int* attackCount) {
+	visit[*attackCount][0] = visit[*attackCount - 1][0] - damage1[damageIndex];
+	visit[*attackCount][1] = visit[*attackCount - 1][1] - damage2[damageIndex];
+	visit[*attackCount][2] = visit[*attackCount - 1][2] - damage3[damageIndex];
 	(*attackCount)++;
 
-	if (min != 1 << 30 && *attackCount >= min) return;
-
-	if (succeed && !fail) {
-		min = min > *attackCount ? *attackCount : min;
+	if (visit[*attackCount - 1][0] <= 0 && visit[*attackCount - 1][1] <= 0 && visit[*attackCount - 1][2] <= 0) {
+		minCnt = minCnt > *attackCount ? *attackCount : minCnt;
 		return;
 	}
 
-	for (int i = index; i < index + n; i++) {
-		DFS(i % n, attackCount);
+	for (int i = 0; i < 6; i++) {
+		if (*attackCount >= minCnt - 1) continue;
+		DFS(i, attackCount);
 		(*attackCount)--;
-		for (int j = index; j < index + n; j++) {
-			arr[j % n] += damage[j - index];
-		}
 	}
 }
 
 int main(void) {
+
 	scanf("%d", &n);
 	for (int i = 0; i < n; i++) {
 		scanf("%d", &arr[i]);
+		visit[0][i] = arr[i];
 	}
-	min = 1 << 30;
-	for (int i = 0; i < n; i++) {
-		int attackCount = 0;
+
+	minCnt = 1 << 30;
+	for (int i = 0; i < 6; i++) {
+		int attackCount = 1;
 		DFS(i, &attackCount);
 	}
-	printf("%d", min);
+
+	printf("%d", minCnt-1);
 	return 0;
 }
