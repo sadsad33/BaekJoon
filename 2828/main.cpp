6bda3c646987669d6fafa8edc773efe1b8ffc722
sadsad33 <bonhyuk33@naver.com>
2024-01-16 23:32:05 +0900

    2828 - 그리디

---
+++ 2828/main.cpp
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstring>
+int n, m, appleNum, totalMoveCnt;
+int targetX;
+int main(void) {
+	scanf("%d%d\n%d", &n, &m, &appleNum);
+	int currentXLeft = 0, currentXRight = m - 1;
+	while (appleNum--) {
+		int x; scanf("%d", &x);
+		targetX = x - 1;
+		if (targetX >= currentXLeft && targetX <= currentXRight) continue;
+		if (abs(targetX - currentXLeft) < abs(targetX - currentXRight)) {
+			totalMoveCnt += abs(targetX - currentXLeft);
+			currentXRight -= abs(targetX - currentXLeft);
+			currentXLeft = targetX;
+		}
+		else {
+			totalMoveCnt += abs(targetX - currentXRight);
+			currentXLeft += abs(targetX - currentXRight);
+			currentXRight = targetX;
+		}
+	}
+
+	printf("%d", totalMoveCnt);
+	return 0;
+}
