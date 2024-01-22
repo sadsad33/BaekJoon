9590105e7f3d55c39cb5f74ba5d63d1f05c3dda7
sadsad33 <bonhyuk33@naver.com>
2024-01-22 23:43:12 +0900

    2170 - 라인 스위핑

---
+++ 2170/main.cpp
@@ -0,0 +1,39 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <algorithm>
+#include <iostream>
+using namespace std;
+int n;
+typedef struct {
+	int start;
+	int end;
+}Line;
+Line lines[1000010];
+
+bool Compare(Line a, Line b) {
+	if (a.start == b.start) return a.end < b.end;
+	return a.start < b.start;
+}
+int main(void) {
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		scanf("%d%d", &lines[i].start, &lines[i].end);
+	}
+
+	sort(lines, lines + n, Compare);
+	int minStart = lines[0].start, maxEnd = lines[0].end;
+	int totalLength = 0;
+	for (int i = 1; i < n; i++) {
+		int newStart = lines[i].start, newEnd = lines[i].end;
+		if (newStart <= maxEnd && newEnd >= maxEnd) {
+			maxEnd = newEnd;
+		}
+		else if (newStart > maxEnd) {
+			totalLength += maxEnd - minStart;
+			minStart = newStart;
+			maxEnd = newEnd;
+		}
+	}
+	totalLength += maxEnd - minStart;
+	printf("%d", totalLength);
+	return 0;
+}
