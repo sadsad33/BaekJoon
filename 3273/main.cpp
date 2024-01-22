ac1b3859b2d39274116644fd019e99f302515821
sadsad33 <bonhyuk33@naver.com>
2024-01-22 23:43:24 +0900

    3273 - 투 포인터

---
+++ 3273/main.cpp
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int arr[100001];
+int main(void) {
+	int n; scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &arr[i]);
+	}
+	int x; scanf("%d", &x);
+	sort(arr, arr + n);
+	int leftIndex = 0, rightIndex = n - 1, cnt = 0;
+	while (leftIndex < rightIndex) {
+		if (arr[leftIndex] + arr[rightIndex] > x) rightIndex--;
+		else if (arr[leftIndex] + arr[rightIndex] < x) leftIndex++;
+		else {
+			leftIndex++; rightIndex--;
+			cnt++;
+		}
+	}
+
+	printf("%d", cnt);
+	return 0;
+}
