c05c3ad6689755a933bbbf6a78e237fa3124b104
sadsad33 <bonhyuk33@naver.com>
2024-01-29 22:43:17 +0900

    11722 LIS

---
+++ 11722/main.cpp
@@ -0,0 +1,27 @@
+// 11722 가장 긴 감소하는 부분 수열
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int arr[1010];
+int main(void) {
+	int n; scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &arr[i]);
+	}
+
+	int dp[1010] = { 0 }, maxLength = 0;
+	for (int i = 0; i < n; i++) {
+		dp[i] = 1;
+		for (int j = 0; j < i; j++) {
+			if (arr[j] > arr[i]) {
+				dp[i] = max(dp[i], dp[j] + 1);
+			}
+		}
+		maxLength = max(maxLength, dp[i]);
+	}
+
+	printf("%d", maxLength);
+	return 0;
+}
