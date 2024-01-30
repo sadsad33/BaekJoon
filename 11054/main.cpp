74d630d36fad67db0a574b2e542909fbb66c0649
sadsad33 <bonhyuk33@naver.com>
2024-01-30 20:25:30 +0900

    11054 - LIS

---
+++ 11054/main.cpp
@@ -0,0 +1,32 @@
+// 11054 가장 긴 바이토닉 부분 수열
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int arr[1010];
+int main(void) {
+	int n; scanf("%d", &n);
+
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &arr[i]);
+	}
+
+	int dp[1010] = { 0 }, maxLength = 0;
+	for (int i = 0; i < n; i++) {
+		dp[i] = 1;
+		for (int j = 0; j < i; j++) {
+			if (arr[i] > arr[j])
+				dp[i] = max(dp[i], dp[j] + 1);
+		}
+	}
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < i; j++) {
+			if (arr[i] < arr[j])
+				dp[i] = max(dp[i], dp[j] + 1);
+		}
+		maxLength = max(dp[i], maxLength);
+	}
+	printf("%d", maxLength);
+	return 0;
+}
