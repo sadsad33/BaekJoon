8b06a8d6057af47137080e53a6d2f43e40a5370f
sadsad33 <bonhyuk33@naver.com>
2024-02-06 22:48:50 +0900

    2343

--- 2343/main.cpp
+++ 2343/main.cpp
@@ -1,5 +1,4 @@
 // 2343 기타 레슨
-#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -7,9 +6,10 @@ using namespace std;
 int n, m;
 int arr[100010];
 int Check(int mid, int maxLength) {
-	if (maxLength > mid) return 0;
+	if (maxLength > mid) return 0; // 임의의 값이 포함 시켜야할 가장 큰 수보다 작으면 실패
 	int temp = mid;
 	int cnt = 0;
+	// 임의로 정한 값에서 배열의 수중 하나씩 빼면서 만약 0보다 작아지면 카운트
 	for (int i = 0; i < n; i++) {
 		if (mid - arr[i] < 0) {
 			mid = temp;
@@ -17,6 +17,7 @@ int Check(int mid, int maxLength) {
 		}
 		mid -= arr[i];
 	}
+	// 모든 수를 뺐는데도 0이 되지않는다면 하나로 묶을수는 있는 것이므로 카운트
 	if (mid != temp) cnt++;
 	return cnt <= m;
 }
@@ -31,7 +32,7 @@ int main(void) {
 
 	int left = 0, right = totalLength, ans;
 	while (left <= right) {
-		int mid = (left + right) / 2;
+		int mid = (left + right) / 2; // 0과 총합 값 사이의 임의의 값을 선택
 		if (Check(mid, maxLength)) {
 			right = mid - 1;
 			ans = mid;
