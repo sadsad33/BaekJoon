5fb86a07ee494dff2f483a634b0c8ae174d0cb30
sadsad33 <bonhyuk33@naver.com>
2024-02-10 20:44:01 +0900

    9461 동적계획법

---
+++ 9461/main.cpp
@@ -0,0 +1,19 @@
+//9461 동적 계획법
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+
+long long dp[110];
+int main(void) {
+	int t; scanf("%d", &t);
+	dp[0] = 0, dp[1] = 1, dp[2] = 1, dp[3] = 1, dp[4] = 2;
+	for (int i = 5; i <= 100; i++) {
+		dp[i] = dp[i - 1] + dp[i - 5];
+	}
+
+	while (t--) {
+		int input; scanf("%d", &input);
+		printf("%lld\n", dp[input]);
+	}
+	return 0;
+}
